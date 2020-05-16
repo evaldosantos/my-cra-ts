522b3ab459b28d0ede8058875d217ce598fc1ead
Evaldo Santos <jesantosjr@protonmail.com>
2020-05-16 19:09:33 +0200

    use different config files for development and production webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -3,26 +3,11 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    filename: 'index.[contentHash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
         test: /\.html$/i,
         loader: 'html-loader',
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
       },
       {
         test: /\.js$/,
---
+++ webpack.production.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'index.[contentHash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+});
