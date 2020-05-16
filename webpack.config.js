4fc5f35aabe7cc3012aef85781e3f832a70e1fbf
Evaldo Santos <jesantosjr@protonmail.com>
2020-05-16 18:52:30 +0200

    use contentHash to avoid cache problems

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    filename: 'index.js',
+    filename: 'index.[contentHash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
