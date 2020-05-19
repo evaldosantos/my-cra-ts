e3dcd70debebce809fa59bdf28888c2d13698b77
Evaldo Santos <jesantosjr@protonmail.com>
2020-05-19 21:04:49 +0200

    reset app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import './style.scss';
 
-const App = () => {
-  const person = { name: 'John Doe' };
-  const { name } = person;
-
-  return <h1>Webpack + babel + react</h1>;
-};
+const App = () => <h1>Webpack + babel + react</h1>;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style.scss';
 
 const wrapper = document.getElementById('container');
 ReactDOM.render(<App />, wrapper);
